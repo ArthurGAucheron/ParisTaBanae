b1a3974963c3a4f7870ee3abb07369665d1c1587
ArthurGaucheron <arthurgaucheron@hotmail.fr>
2022-05-23 18:47:19 +0200

    Initialisation du projet back office

---
+++ back_office/src/test/java/com/artga/ToBetOrNotToBet/ToBetOrNotToBetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artga.ToBetOrNotToBet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToBetOrNotToBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
