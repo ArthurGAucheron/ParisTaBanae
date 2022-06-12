b20b8d77cc71266592ce94958159b134035ec042
ArthurGaucheron <arthurgaucheron@hotmail.fr>
2022-06-12 12:03:02 +0200

    Ajout de la datasource dans le fichier application.properties.

--- back_office/src/main/java/com/artga/ToBetOrNotToBet/SpringBootLauncher.java
+++ back_office/src/main/java/com/artga/ToBetOrNotToBet/SpringBootLauncher.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToBetOrNotToBetApplication {
+public class SpringBootLauncher {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToBetOrNotToBetApplication.class, args);
+		SpringApplication.run(SpringBootLauncher.class, args);
 	}
 
 }
